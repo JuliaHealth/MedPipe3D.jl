865b2886e6b92c92a4daccc9dc37bfee3ebcb056
Jakub Mitura <jakub.mitura14@gmail.com>
2022-06-30 21:14:22 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/visualizationUtils/visualizationFromHdf5.jl
+++ src/visualizationUtils/visualizationFromHdf5.jl
@@ -38,7 +38,7 @@ using ..distinctColorsSaved
 
 using  MedEye3d.OpenGLDisplayUtils,  MedEye3d.ForDisplayStructs
 using   MedEye3d.Uniforms,   MedEye3d.CustomFragShad,  MedEye3d.DataStructs,  MedEye3d.DisplayWords
-using   MedEye3d.OpenGLDisplayUtils,  MedEye3d.ForDisplayStructs
+using   MedEye3d.OpenGLDisplayUtils,  MedEye3d.ForDisplayStructs,MedEye3d.TextureManag
 using MedEval3D
 using MedEval3D.BasicStructs
 using MedEval3D.MainAbstractions
