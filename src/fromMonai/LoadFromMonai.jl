0d65d1ad28d9506dde6b9492274c391a32d71059
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-13 14:54:19 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/fromMonai/LoadFromMonai.jl
+++ src/fromMonai/LoadFromMonai.jl
@@ -73,9 +73,9 @@ function resamplesitkImageTosize(image,targetSpac)
     orig_spacing=image.GetSpacing()
     origSize =image.GetSize()
 
-    new_size = (round(origSize[1]*(orig_spacing[1]/targetSpac[1])),
-    round(origSize[2]*(orig_spacing[2]/targetSpac[2])),
-    round(origSize[3]*(orig_spacing[3]/targetSpac[3]) )    )
+    new_size = ((origSize[1]*(orig_spacing[1]/targetSpac[1])),
+    (origSize[2]*(orig_spacing[2]/targetSpac[2])),
+    (origSize[3]*(orig_spacing[3]/targetSpac[3]) )    )
 
     resample = sitk.ResampleImageFilter()
     resample.SetOutputSpacing(targetSpac)
