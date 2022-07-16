02a6d81f336e1c2a497a1bf41731d697ee994938
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-16 18:19:51 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/fromMonai/LoadFromMonai.jl
+++ src/fromMonai/LoadFromMonai.jl
@@ -178,8 +178,8 @@ function loadandPad(
     image=resamplesitkImageTosize(image,targetSpacing,sitk)
     label=resamplesitkImageTosize(label,targetSpacing,sitk)
     
-    padToSize(image,targetSize, 0,sitk)
-    padToSize(label,targetSize, 0,sitk)
+    image=padToSize(image,targetSize, 0,sitk)
+    label=padToSize(label,targetSize, 0,sitk)
 
     imageArr=permuteAndReverseFromSitk(pyconvert(Array,sitk.GetArrayFromImage(image)))
     labelArr=permuteAndReverseFromSitk(pyconvert(Array,sitk.GetArrayFromImage(label)))
